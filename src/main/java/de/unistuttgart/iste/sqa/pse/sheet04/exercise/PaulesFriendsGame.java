8be8b4c02a8f5a00e4bd324ab1884848fd98647e
MariusHaueis <74296842+MariusHaueis@users.noreply.github.com>
2021-11-03 12:43:58 +0100

    Updated the imports

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet04/exercise/PaulesFriendsGame.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet04/exercise/PaulesFriendsGame.java
@@ -1,16 +1,22 @@
 package de.unistuttgart.iste.sqa.pse.sheet04.exercise;
 
-public class PaulesFriendsGame extends Exercise04Base {
+import de.hamstersimulator.objectsfirst.datatypes.Direction;
+import de.hamstersimulator.objectsfirst.datatypes.Location;
+import de.hamstersimulator.objectsfirst.external.model.Hamster;
 
+
+
+public class PaulesFriendsGame extends Exercise04Base{
+	
 	/**
 	 * Put your hamster code into this operation.
 	 */
 	void createPaulesFriends() {
 		// TODO: implement according to exercise 2 of sheet 4
 		
-		
 		// Hint: Be careful with the location of the new hamsters
-
+		
+		
 	}
 
 	/**
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,7 @@
 open module exercise {
     requires transitive de.hamstersimulator.objectsfirst.inspector;
 	requires javafx.graphics;
+	requires de.hamstersimulator.objectsfirst.main;
+	requires de.hamstersimulator.objectsfirst.core;
     exports de.unistuttgart.iste.sqa.pse.sheet04.exercise;
 }
