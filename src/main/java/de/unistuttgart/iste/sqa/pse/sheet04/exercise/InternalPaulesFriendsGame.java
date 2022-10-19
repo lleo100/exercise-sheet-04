a044a478f5541891d7d10a0a1408456d1df7c033
Henrik Möllmann <henmoell@gmail.com>
2022-10-19 15:22:33 +0200

    added missing JavaDoc

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet04/exercise/InternalPaulesFriendsGame.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet04/exercise/InternalPaulesFriendsGame.java
@@ -3,9 +3,13 @@ package de.unistuttgart.iste.sqa.pse.sheet04.exercise;
 import de.hamstersimulator.objectsfirst.inspector.InspectableSimpleHamsterGame;
 
 /**
- * Creates and runs Hamster-Simulator for exercise 2 in part A.
+ * Class for creating and running the Hamster-Simulator for exercise 2 in part A.
  */
 public abstract class InternalPaulesFriendsGame extends InspectableSimpleHamsterGame {
+
+	/**
+	 * Creates and runs Hamster-Simulator for exercise 2 in part A.
+	 */
 	public InternalPaulesFriendsGame() {
 		this.loadTerritoryFromResourceFile("/territories/territory-ub04.ter");
 		this.displayInNewGameWindow();
