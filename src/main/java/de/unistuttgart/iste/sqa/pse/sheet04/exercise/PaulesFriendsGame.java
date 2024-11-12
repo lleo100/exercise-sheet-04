d4d95179b413f6b463525a4839d996d59210a32a
Leo Bäuerlein <109874436+lleo100@users.noreply.github.com>
2024-11-12 08:44:28 +0100

    Update PaulesFriendsGame.java Imports
    Import der Hamster Klasse hat gefehlt

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet04/exercise/PaulesFriendsGame.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet04/exercise/PaulesFriendsGame.java
@@ -3,6 +3,7 @@ package de.unistuttgart.iste.sqa.pse.sheet04.exercise;
 import de.hamstersimulator.objectsfirst.datatypes.Direction;
 import de.hamstersimulator.objectsfirst.datatypes.Location;
 import de.hamstersimulator.objectsfirst.external.model.Territory;
+import de.hamstersimulator.objectsfirst.external.model.Hamster;
 
 /**
  * In this hamster-game {@code paule} gets two new friends.
